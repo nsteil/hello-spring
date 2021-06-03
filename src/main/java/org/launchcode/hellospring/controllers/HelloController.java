97675e68ed54bdba5c317cac2ec3c0acba4db7a3
Nate Steil <nathanrsteil@gmail.com>
2021-06-03 11:21:06 -0500

    completed the Java Thymelead Part 1 video

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -9,8 +9,8 @@ import javax.websocket.server.PathParam;
  * Created by Nate Steil
  */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+
+
 public class HelloController {
 
     // Handles request at path /hello
@@ -22,15 +22,15 @@ public class HelloController {
 
     //now lives at /hello/goodbye due to the request mapping above the class
     @GetMapping("goodbye")
-
+    @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring";
     }
 
     // Handles requests of the form /hello?name=LaunchCode
 
-    @RequestMapping(method= {RequestMethod.GET, RequestMethod.POST}) // Allows us to use both get and post
-
+    @RequestMapping(value ="hello", method= {RequestMethod.GET, RequestMethod.POST}) // Allows us to use both get and post
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "! ";
     }
@@ -38,24 +38,15 @@ public class HelloController {
     // Handles requests of the form /hello/LaunchCode
 
     @GetMapping("{name}")
-
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
     //now lives at /hello/form
     @GetMapping("form")
+    public String helloForm() {
+        return "form";
 
-    public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
     }
-
-
 }
